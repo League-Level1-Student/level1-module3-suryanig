de5fc52997ff4bb87c389e400cc4585a5c3ea606
suryanig <67238881+suryanig@users.noreply.github.com>
2021-07-12 18:25:42 -0700

    I finished Smiley Blues

--- src/_01_static_smiley_blues/SmileyBluesRunner.java
+++ src/_01_static_smiley_blues/SmileyBluesRunner.java
@@ -1,8 +1,22 @@
 package _01_static_smiley_blues;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class SmileyBluesRunner {
 	public static void main(String[] args) {
 		SmileyBlues smileyRunner = new SmileyBlues();
 		smileyRunner.start();
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		Smiley smiley = new Smiley();
+		Smiley smile = new Smiley();
+		Smiley smil = new Smiley();
+		panel.add(smiley);
+		panel.add(smile);
+		panel.add(smil);
+		frame.pack();
 	}
 }
