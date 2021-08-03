f5e969b70695f3dcaa4cab54ad51eff1406abe81
suryanig <67238881+suryanig@users.noreply.github.com>
2021-08-02 18:29:49 -0700

    I am almost done with gridworld.

--- src/_06_gridworld/GridworldRunner.java
+++ src/_06_gridworld/GridworldRunner.java
@@ -1,10 +1,23 @@
 package _06_gridworld;
 
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
 public class GridworldRunner {
 	public static void main(String[] args) {
-		public void show() {
-			
-		}
-		public void add(bug)
+World world = new World();
+world.show();
+Bug a = new Bug();
+Location loc = new Location(5,5);
+world.add(loc, a);
+Bug b = new Bug();
+Location loc1 = new Location(3,5);
+world.add(loc1, b);
+b.setColor(Color.blue);
+b.turn();
+b.turn();
 	}
 }
